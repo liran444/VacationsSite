8fe938ae3dc55f1da4ceb50484a6c8ac8786ca70
liran444 <liran.kim@gmail.com>
2020-09-20 01:15:58 +0300

    fix

--- VacationsNode/dao/connection-wrapper.js
+++ VacationsNode/dao/connection-wrapper.js
@@ -67,7 +67,8 @@ function handleDisconnect() {
       handleDisconnect();                         // lost due to either server restart, or a
     } else {                                      // connnection idle timeout (the wait_timeout
       throw err;                                 // server variable configures this)
-    });
+    }
+  });
 }
 
 handleDisconnect();
