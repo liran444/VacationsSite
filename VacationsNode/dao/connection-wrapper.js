8a477265147b57c68d7680a8a9f77739e9b2f01a
liran444 <liran.kim@gmail.com>
2020-09-20 01:21:09 +0300

    handleDisconnect

--- VacationsNode/dao/connection-wrapper.js
+++ VacationsNode/dao/connection-wrapper.js
@@ -59,7 +59,7 @@ function handleDisconnect() {
     connection.connect(function (err) {              // The server is either down
       if (err) {                                     // or restarting (takes a while sometimes).
         console.log('error when connecting to db:', err);
-        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        setTimeout(handleDisconnect, 5000); // We introduce a delay before attempting to reconnect,
       }                                     // to avoid a hot loop, and to allow our node script to
     });                                     // process asynchronous requests in the meantime.
     // If you're also serving http, display a 503 error.
