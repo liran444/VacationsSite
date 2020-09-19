ee411909db3f7fffd4286042a30bca76c3ad76e0
liran444 <liran.kim@gmail.com>
2020-09-20 01:22:31 +0300

    handleDisconnect

--- VacationsNode/dao/connection-wrapper.js
+++ VacationsNode/dao/connection-wrapper.js
@@ -62,15 +62,15 @@ function handleDisconnect() {
         setTimeout(handleDisconnect, 5000); // We introduce a delay before attempting to reconnect,
       }                                     // to avoid a hot loop, and to allow our node script to
     });                                     // process asynchronous requests in the meantime.
-    // If you're also serving http, display a 503 error.
-    connection.on('error', function (err) {
-      console.log('db error', err);
-      if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-        handleDisconnect();                         // lost due to either server restart, or a
-      } else {                                      // connnection idle timeout (the wait_timeout
-        throw err;                                 // server variable configures this)
-      }
-    });
+    // // If you're also serving http, display a 503 error.
+    // connection.on('error', function (err) {
+    //   console.log('db error', err);
+    //   if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+    //     handleDisconnect();                         // lost due to either server restart, or a
+    //   } else {                                      // connnection idle timeout (the wait_timeout
+    //     throw err;                                 // server variable configures this)
+    //   }
+    // });
 
   } catch (e) {
     console.log(`[handleDisconnect] - error: ${JSON.stringify(e)}`)
