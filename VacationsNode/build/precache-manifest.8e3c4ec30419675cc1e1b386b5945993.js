bd8792b939da5aa710645457c25bcb259565a18b
liran444 <liran.kim@gmail.com>
2020-09-19 23:42:55 +0300

    Updated react build

---
+++ VacationsNode/build/precache-manifest.8e3c4ec30419675cc1e1b386b5945993.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e42707b26c2ed53ee5af3cbe59a7da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a39206caf5efc2385d4",
+    "url": "/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "7b1d9b50715d39d55ea7",
+    "url": "/static/css/main.9856b271.chunk.css"
+  },
+  {
+    "revision": "2a39206caf5efc2385d4",
+    "url": "/static/js/2.1ee33991.chunk.js"
+  },
+  {
+    "revision": "5b23e0fffb7a7b78b05b650c531aa371",
+    "url": "/static/js/2.1ee33991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b1d9b50715d39d55ea7",
+    "url": "/static/js/main.46969d19.chunk.js"
+  },
+  {
+    "revision": "59d005248bf9897105c4",
+    "url": "/static/js/runtime-main.24666b67.js"
+  }
+]);
